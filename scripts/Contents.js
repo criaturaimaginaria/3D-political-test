3c8dac4e23b26d837fcdb8de116757da59dbd6e4
Valentin <valkjavalkja@gmail.com>
2021-11-04 14:40:05 -0300

    symbols path changed

--- scripts/Contents.js
+++ scripts/Contents.js
@@ -3,55 +3,55 @@ let conservador =  {
         0: {
           name : "Comunismo cristiano",
           description: "El comunismo cristiano es una forma de comunismo religioso basado en el cristianismo. Es una teoría teológica y política basada en las enseñanzas de Jesucristo que llevaría a los cristianos a apoyar el comunismo como sistema social ideal",
-          symbol: './symbols/comunismoCristiano.png'
+          symbol: './styles/symbols/comunismoCristiano.png'
         },
 
         1: {
           name: "Socialismo cristiano",
           description: "fddddddddddddddddddddddddddddddddlpfldps",
-          symbol: './symbols/socialismoCristiano.png'
+          symbol: './styles/symbols/socialismoCristiano.png'
         },
 
         2: {
           name: "Anarquismo cristiano",
           description: "El anarquismo cristiano o anarcocristianismo es una forma de entender la vida cristiana en la que los principios éticos y de organización social son anarquistas. Los cristianos anarquistas afirman que si cada individuo es valioso ante Dios, solo rige el derecho natural de las personas como fundamento de la libertad individual y no hay lugar para gobiernos, civiles o eclesiásticos, sobre las personas",
-          symbol: './symbols/cristianAncap.png'
+          symbol: './styles/symbols/cristianAncap.png'
         },
 
         3: {
           name: "Fascismo clerical",
           description: "buscar info, es franco, pero bueno, tampoo encontré foto de sus simbolos",
-          symbol: './symbols/sin imagen por ahora.png'
+          symbol: './styles/symbols/sin imagen por ahora.png'
         },
 
         4: {
           name: "Centro derecha.",
           description: "las personas de centro derecha bla bla bla",
-          symbol: './symbols/sin imagen por ahora.png'
+          symbol: './styles/symbols/sin imagen por ahora.png'
         },
 
         5: {
           name: "Anarco conservadurismo.",
           description: "el anarco conservadurismo se basa en bla bla",
-          symbol: './symbols/anarcoConservadurismo.png'
+          symbol: './styles/symbols/anarcoConservadurismo.png'
         },
 
         6: {
           name: "Pinochetismo.",
           description: "el pinochetismo bla bla",
-          symbol: './symbols/ no se cual es el simbolo'
+          symbol: './styles/symbols/ no se cual es el simbolo'
         },
 
         7: {
           name: "Liberalismo conservador.",
           description: "el pinochetismo bla bla",
-          symbol: './symbols/ no se cual es el simbolo'
+          symbol: './styles/symbols/ no se cual es el simbolo'
         },
 
         8: {
           name: "Paleo libertarismo.",
           description: "el paleo libertario es como un liberal pero con una vision mas conservadora etc",
-          symbol: './symbols/paleoLibertario.png'
+          symbol: './styles/symbols/paleoLibertario.png'
         },
 }
 
@@ -61,55 +61,55 @@ socialMiddle =  {
   0: {
     name : "Marxismo clásico",
     description: "El marxismo comienza con Marx y bla bla",
-    symbol: './symbols/marxismoClasico.png'
+    symbol: './styles/symbols/marxismoClasico.png'
   },
 
   1: {
     name: "Socialismo democrático",
     description: "socialismo democrático, o social democrácia es cono bla blas",
-    symbol: './symbols/socialDemocracia.png'
+    symbol: './styles/symbols/socialDemocracia.png'
   },
 
   2: {
     name: "Anarco colectivismo",
     description: "tiene que ver con bakunin, pero qu ediferencia hay con el mutualismo ?",
-    symbol: './symbols/fdff.png'
+    symbol: './styles/symbols/fdff.png'
   },
 
   3: {
     name: "Fascismo clásico",
     description: "es básicamente mussolini el ducce",
-    symbol: './symbols/classicFascism.png'
+    symbol: './styles/symbols/classicFascism.png'
   },
 
   4: {
     name: "Centro",
     description: "las personas de centro  bla bla bla",
-    symbol: './symbols/sin imagen por ahora.png'
+    symbol: './styles/symbols/sin imagen por ahora.png'
   },
 
   5: {
     name: "Mutualismo",
     description: "el mutualismo  se basa en bla bla",
-    symbol: './symbols/mutualismo.png'
+    symbol: './styles/symbols/mutualismo.png'
   },
 
   6: {
     name: "capitalismo iliberal.",
     description: "el es cuando no hay liberatad indivigdual bla bla",
-    symbol: './symbols/ no se cual es el simbolo'
+    symbol: './styles/symbols/ no se cual es el simbolo'
   },
 
   7: {
     name: "liberalismo Clásico.",
     description: "defensa de la propiedad privada , libre comercio y libertad personal",
-    symbol: './symbols/gadsden.png'
+    symbol: './styles/symbols/gadsden.png'
   },
 
   8: {
     name: " libertarismo.",
     description: "el  libertario es como un liberal etc",
-    symbol: './symbols/libertarian.png'
+    symbol: './styles/symbols/libertarian.png'
   },
 
     
@@ -121,55 +121,55 @@ progresista =  {
   0: {
     name : "Marxismo leninismo",
     description: "El marxismo comienza con Marx y bla bla",
-    symbol: './symbols/marxismoClasico.png'
+    symbol: './styles/symbols/marxismoClasico.png'
   },
 
   1: {
     name: "Luxemburguismo",
     description: "El término luxemburguismo se refiere al movimiento marxista revolucionario creado por Rosa Luxemburgo y Karl Liebknecht,",
-    symbol: './symbols/marxismoClasico.png'
+    symbol: './styles/symbols/marxismoClasico.png'
   },
 
   2: {
     name: "Anarco comunismo",
     description: "el anarco comunismo es bla bla",
-    symbol: './symbols/anarcoComunismo.png'
+    symbol: './styles/symbols/anarcoComunismo.png'
   },
 
   3: {
     name: "Nacional Socialismo",
     description: "es basicamento lo de hitler bla bla bla",
-    symbol: './symbols/nacionalSocialismo.png'
+    symbol: './styles/symbols/nacionalSocialismo.png'
   },
 
   4: {
     name: "Centro izquierda",
     description: "las personas de centro izq bab bla  bla bla bla",
-    symbol: './symbols/sin imagen por ahora.png'
+    symbol: './styles/symbols/sin imagen por ahora.png'
   },
 
   5: {
     name: "anarquismo egoista",
     description: "el anarquismo egoista bla blala",
-    symbol: './symbols/anarquismoEgoista.png'
+    symbol: './styles/symbols/anarquismoEgoista.png'
   },
 
   6: {
     name: "tecnocracia.",
     description: "vblavlavlalasbla",
-    symbol: './symbols/ no se cual es el simbolo'
+    symbol: './styles/symbols/ no se cual es el simbolo'
   },
 
   7: {
     name: "Socio Liberalismo.",
     description: "defensa de la propiedad privada , libre comercio y libertad personal son lagunas cosas sociales",
-    symbol: './symbols/socioLiberalismo.png'
+    symbol: './styles/symbols/socioLiberalismo.png'
   },
 
   8: {
     name: " Agorismo.",
     description: "el agorismo se trata de llegar a la anarquia meiante  la contra eocnomia bla bla",
-    symbol: './symbols/agorismo.png'
+    symbol: './styles/symbols/agorismo.png'
   },
 }
 
