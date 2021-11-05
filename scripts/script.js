0576daef1e82d741970f15e95b73f68e18870c1b
Valentin <valkjavalkja@gmail.com>
2021-11-04 22:55:47 -0300

    mayor o igual

--- scripts/script.js
+++ scripts/script.js
@@ -56,139 +56,139 @@ function pointPosition (myScore, VeScore, socScore) {
 //here we get the content number to display the data from the content.js file ------
 function contentNumber (myScore, VeScore, socScore) { 
         // if for social score conservative |||||||||||||||||||||||||
-      if(socScore < 6.6 && myScore < 6 &&  VeScore > 14 ){
+      if(socScore <= 6.6 && myScore <= 6 &&  VeScore >= 14 ){
         contNumber = 0
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore < 6 &&  VeScore > 7 &&  VeScore < 14){
+      else if(socScore <= 6.6 && myScore <= 6 &&  VeScore >= 7 &&  VeScore <= 14){
         contNumber = 1
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore < 6 &&  VeScore < 6 ){
+      else if(socScore <= 6.6 && myScore <= 6 &&  VeScore <= 6 ){
         contNumber = 2
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore > 7 &&  myScore < 13 && VeScore > 14 ){
+      else if(socScore <= 6.6 && myScore >= 7 &&  myScore <= 13 && VeScore >= 14 ){
         contNumber = 3
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore > 7 &&  myScore < 13 && VeScore > 7 && VeScore < 13 ){
+      else if(socScore <= 6.6 && myScore >= 7 &&  myScore <= 13 && VeScore >= 7 && VeScore <= 13 ){
         contNumber = 4
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore > 7 &&  myScore < 13 && VeScore < 7 ){
+      else if(socScore <= 6.6 && myScore >= 7 &&  myScore <= 13 && VeScore <= 7 ){
         contNumber = 5
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore > 14  && VeScore > 14 ){
+      else if(socScore <= 6.6 && myScore >= 14  && VeScore >= 14 ){
         contNumber = 6
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore > 14  && VeScore < 14 && VeScore > 7  ){
+      else if(socScore <= 6.6 && myScore >= 14  && VeScore <= 14 && VeScore >= 7  ){
         contNumber = 7
         socArrow = conservador
       }
 
-      else if(socScore < 6.6 && myScore > 14  && VeScore < 7  ){
+      else if(socScore <= 6.6 && myScore >= 14  && VeScore <= 7  ){
         contNumber = 8
         socArrow = conservador
       }
 
       // if for social score middle, |||||||||||||||||||
-      else if(socScore > 6 && socScore < 13 && myScore < 6 &&  VeScore > 14 ){
+      else if(socScore >= 6 && socScore <= 13 && myScore <= 6 &&  VeScore >= 14 ){
         contNumber = 0
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13  && myScore < 6 &&  VeScore > 7 &&  VeScore < 14){
+      else if(socScore >= 6 && socScore <= 13  && myScore <= 6 &&  VeScore >= 7 &&  VeScore <= 14){
         contNumber = 1
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13  && myScore < 6 &&  VeScore < 6 ){
+      else if(socScore >= 6 && socScore <= 13  && myScore <= 6 &&  VeScore <= 6 ){
         contNumber = 2
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13 && myScore > 7 &&  myScore < 13 && VeScore > 14 ){
+      else if(socScore >= 6 && socScore <= 13 && myScore >= 7 &&  myScore <= 13 && VeScore >= 14 ){
         contNumber = 3
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13  && myScore > 6.9 &&  myScore < 14 && VeScore > 7 && VeScore < 13 ){
+      else if(socScore >= 6 && socScore <= 13  && myScore >= 6.9 &&  myScore <= 14 && VeScore >= 7 && VeScore <= 13 ){
         contNumber = 4
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13 && myScore > 7 &&  myScore < 13 && VeScore < 7 ){
+      else if(socScore >= 6 && socScore <= 13 && myScore >= 7 &&  myScore <= 13 && VeScore <= 7 ){
         contNumber = 5
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13  && myScore > 14  && VeScore > 14 ){
+      else if(socScore >= 6 && socScore <= 13  && myScore >= 14  && VeScore >= 14 ){
         contNumber = 6
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13  && myScore > 13   && VeScore > 6.8 && VeScore < 14 ){
+      else if(socScore >= 6 && socScore <= 13  && myScore >= 13   && VeScore >= 6.8 && VeScore <= 14 ){
         contNumber = 7
         socArrow = socialMiddle
       }
 
-      else if(socScore > 6 && socScore < 13  && myScore > 14  && VeScore < 7  ){
+      else if(socScore >= 6 && socScore <= 13  && myScore >= 14  && VeScore <= 7  ){
         contNumber = 8
         socArrow = socialMiddle
       }
 
         // if for social score progresist  ||||||||||||||||||
-        else if(socScore > 13  && myScore < 6.5 &&  VeScore > 14 ){
+        else if(socScore >= 13  && myScore <= 6.5 &&  VeScore >= 14 ){
           contNumber = 0
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore < 6.5 &&  VeScore > 7 &&  VeScore < 14){
+        else if(socScore >= 13 && myScore <= 6.5 &&  VeScore >= 7 &&  VeScore <= 14){
           contNumber = 1
           socArrow = progresista
         }
   
-        else if(socScore > 13&& myScore < 6.5 &&  VeScore < 6 ){
+        else if(socScore >= 13&& myScore <= 6.5 &&  VeScore <= 6 ){
           contNumber = 2
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore > 7 &&  myScore < 13 && VeScore > 14 ){
+        else if(socScore >= 13 && myScore >= 7 &&  myScore <= 13 && VeScore >= 14 ){
           contNumber = 3
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore > 7 &&  myScore < 13 && VeScore > 7 && VeScore < 13 ){
+        else if(socScore >= 13 && myScore >= 7 &&  myScore <= 13 && VeScore >= 7 && VeScore <= 13 ){
           contNumber = 4
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore > 7 &&  myScore < 13 && VeScore < 7 ){
+        else if(socScore >= 13 && myScore >= 7 &&  myScore <= 13 && VeScore <= 7 ){
           contNumber = 5
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore > 14  && VeScore > 14 ){
+        else if(socScore >= 13 && myScore >= 14  && VeScore >= 14 ){
           contNumber = 6
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore > 14  && VeScore < 14 && VeScore > 7  ){
+        else if(socScore >= 13 && myScore >= 14  && VeScore <= 14 && VeScore >= 7  ){
           contNumber = 7
           socArrow = progresista
         }
   
-        else if(socScore > 13 && myScore > 14  && VeScore < 7  ){
+        else if(socScore >= 13 && myScore >= 14  && VeScore <= 7  ){
           contNumber = 8
           socArrow = progresista
         }
@@ -216,7 +216,7 @@ function total() {
            socScore = socScore + socialPoints[i];
         } 
 
-        console.log(VeScore, VeScore, socScore);
+        console.log(myScore, VeScore, socScore);
         pointPosition(myScore, VeScore, socScore);   
       contentNumber(myScore, VeScore, socScore);     
   
