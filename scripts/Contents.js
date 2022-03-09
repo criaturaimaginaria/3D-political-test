8bef10d8da9473fa1cda26dab98a7d62eacbd94f
Valentin <valkjavalkja@gmail.com>
2022-03-09 19:38:25 -0300

    new imgs and lgbt opction changed a bit

--- scripts/Contents.js
+++ scripts/Contents.js
@@ -3,55 +3,55 @@ let conservador =  {
         0: {
           name : "Nacional bolchevismo",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/comunismoCristiano.png'
+          symbol: './styles/symbols/Nacional-Bolchevismo.png'
         },
 
         1: {
           name: "Socialismo cristiano",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/socialismoCristiano.png'
+          symbol: './styles/symbols/socialismo-cristiano.png'
         },
 
         2: {
           name: "Anarquismo cristiano",
           description: "El anarquismo cristiano o anarcocristianismo es una forma de entender la vida cristiana en la que los principios éticos y de organización social son anarquistas. Los cristianos anarquistas afirman que si cada individuo es valioso ante Dios, solo rige el derecho natural de las personas como fundamento de la libertad individual y no hay lugar para gobiernos, civiles o eclesiásticos, sobre las personas",
-          symbol: './styles/symbols/cristianAncap.png'
+          symbol: './styles/symbols/Anarquismo-cristiano.png'
         },
 
         3: {
           name: "Fascismo clerical",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/sin imagen por ahora.png'
+          symbol: './styles/symbols/Fascismo-clerical.png'
         },
 
         4: {
           name: "Centro derecha.",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/sin imagen por ahora.png'
+          symbol: './styles/symbols/Centro-derecha.png'
         },
 
         5: {
           name: "comunismo tradicionalista.",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/anarcoConservadurismo.png'
+          symbol: './styles/symbols/Comunalismo-tradicionalista.png'
         },
 
         6: {
           name: "Neoreacción (Nrx).",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/ no se cual es el simbolo'
+          symbol: './styles/symbols/Nrx.png'
         },
 
         7: {
           name: "Liberalismo conservador.",
           description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          symbol: './styles/symbols/ no se cual es el simbolo'
+          symbol: './styles/symbols/Liberalismo Conservador.png'
         },
 
         8: {
           name: "Paleo libertarismo.",
           description: "el paleo libertario es como un liberal pero con una vision mas conservadora etc",
-          symbol: './styles/symbols/paleoLibertario.png'
+          symbol: './styles/symbols/Paleolibertarismo.png'
         },
 }
 
@@ -61,55 +61,55 @@ socialMiddle =  {
   0: {
     name : "Marxismo leninismo",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/marxismoClasico.png'
+    symbol: './styles/symbols/Marxismo-leninismo.png'
   },
 
   1: {
     name: "Socialismo democrático",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/socialDemocracia.png'
+    symbol: './styles/symbols/Socialismo-democrático.png'
   },
 
   2: {
     name: "Anarco comunismo",
     description: "tel anarco comunismo bla bla",
-    symbol: './styles/symbols/f fffdff.png'
+    symbol: './styles/symbols/Anarco-comunismo.png'
   },
 
   3: {
     name: "Fascismo clásico",
     description: "es básicamente mussolini el ducce ",
-    symbol: './styles/symbols/classicFascism.png'
+    symbol: './styles/symbols/Fascismo.png'
   },
 
   4: {
     name: "Centro",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/sin imagen por ahora.png'
+    symbol: './styles/symbols/Centro.png'
   },
 
   5: {
     name: "Mutualismo",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/mutualismo.png'
+    symbol: './styles/symbols/Mutualismo.png'
   },
 
   6: {
     name: "capitalismo iliberal.",
     description: "el es cuando no hay liberatad indivigdual bla bla",
-    symbol: './styles/symbols/ no se cual es el simbolo'
+    symbol: './styles/symbols/Capitalismo-iliberal.png'
   },
 
   7: {
     name: "liberalismo Clásico.",
     description: "defensa de la propiedad privada , libre comercio y libertad personal",
-    symbol: './styles/symbols/gadsden.png'
+    symbol: './styles/symbols/Liberalismo-clásico.png'
   },
 
   8: {
     name: " libertarismo.",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/libertarian.png'
+    symbol: './styles/symbols/Libertarismo.png'
   },
 
     
@@ -121,55 +121,55 @@ progresista =  {
   0: {
     name : "Comunismo posmarxista",
     description: "El marxismo comienza con Marx y bla bla",
-    symbol: './styles/symbols/marxismoClasico.png'
+    symbol: './styles/symbols/Pos-marxismo.png'
   },
 
   1: {
     name: "Luxemburguismo",
     description: "El término luxemburguismo se refiere al movimiento marxista revolucionario creado por Rosa Luxemburgo y Karl Liebknecht,",
-    symbol: './styles/symbols/marxismoClasico.png'
+    symbol: './styles/symbols/Luxemburgismo.png'
   },
 
   2: {
     name: "posanarquismo",
     description: "el anarco comunismo es bla bla",
-    symbol: './styles/symbols/anarcoComunismo.png'
+    symbol: './styles/symbols/Pos-anarquismo.png'
   },
 
   3: {
     name: "Tecnocracia progresista",
     description: "es basicamento techno bla bla bla",
-    symbol: './styles/symbols/nacionalSocialismo.png'
+    symbol: './styles/symbols/Tecnocracia-progresista.png'
   },
 
   4: {
     name: "Centro izquierda",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/sin imagen por ahora.png'
+    symbol: './styles/symbols/Centro-izquierda.png'
   },
 
   5: {
     name: "anarquismo egoista",
     description: "el anarquismo egoista bla blala",
-    symbol: './styles/symbols/anarquismoEgoista.png'
+    symbol: './styles/symbols/Anarquismo-egoísta.png'
   },
 
   6: {
     name: "Jacobinismo.",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/ no se cual es el simbolo'
+    symbol: './styles/symbols/Jacobinismo.png'
   },
 
   7: {
     name: "Socio Liberalismo.",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/socioLiberalismo.png'
+    symbol: './styles/symbols/Socioliberalismo.png'
   },
 
   8: {
     name: "Izquierda libertaria.",
     description: "Lo siento aún no tenemos una descripción para esta ideología. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    symbol: './styles/symbols/agorismo.png'
+    symbol: './styles/symbols/Izquierda-libertaria.png'
   },
 }
 
